1a457a661e43aea9541995b684ac88ae02b1c61c
Adity012 <149262704+Adity012@users.noreply.github.com>
2023-11-15 19:28:47 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,3 @@
-api_id = "21609776"
-api_hash = "61f2558d634df353a2a9806661f4b091"
-bot_token = "6435141772:AAHynl9w9v1om4J_sBsOmIklRI4-aO1UY7Y"
+api_id = "26368249"
+api_hash = "ac2db262d5eb7ce0efc0162404c8d172"
+bot_token = "6370708272:AAFZmBMrKFZKJKWBoS3BQKWQP6mL6U8u-NM"
